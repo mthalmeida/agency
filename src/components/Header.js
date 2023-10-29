a7692d4c215c7e5321f433ae25af92e05eaf9612
mthalmeida <matheus.almeida.neves@gmail.com>
2023-10-29 13:34:40 -0300

    feat: sugindo logo editada

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,14 @@ export default class Header extends React.Component {
     return (
       <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="Inicio">
             <img
-              src="./Images/logo-branca.png"
+              src="https://raw.githubusercontent.com/mthalmeida/agency/main/src/components/Images/logo-branca.png"
               alt="Logo"
-              width="30"
-              height="24"
+              width="3000"
+              height="2400"
               class="d-inline-block align-text-top"
             />
-            Bootstrap
           </a>
         </div>
       </nav>
